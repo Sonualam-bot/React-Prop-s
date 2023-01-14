a869649e72050585639561123f56c1e87c8db1be
Sonu <sonualam1599@gmail.com>
2023-01-14 23:01:31 +0530

    mapping data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
-import Avatar from "./Avatar";
+
+function createCard(contact) {
+  return (
+    <Card
+      key={contact.id}
+      id={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
 
-      <Avatar
-        img="https://avatars.githubusercontent.com/u/61339832?v=4"
-      />
+      {contacts.map(createCard)}
 
-      <Card
+      {/* <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -29,7 +39,7 @@ function App() {
         img={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 
-
 function Avatar(props) {
-  return (
-    <div>
-      <img className="circle-img" src={props.img} alt="avatar_img" />
-    </div>
-  );
+  return <img className="circle-img" src={props.img} alt="avatar_img" />;
 }
 
 export default Avatar;
